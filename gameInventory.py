e094936b55e9541180f201082280f0531b9209bc
weiliya <viktoria.szabo.cv@gmail.com>
2017-06-25 23:30:21 +0200

    almost done

--- gameInventory.py
+++ gameInventory.py
@@ -3,6 +3,7 @@
 
 import csv
 
+
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} 
 added_items = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 order = ""
@@ -49,21 +50,26 @@ def print_table(inventory, order=None):
     print("Total number of items:", sum(inventory.values()))
 
 
+filename = ""
 # Imports new inventory items from a file
 # The filename comes as an argument, but by default it's 
 # "import_inventory.csv". The import automatically merges items by name.
 # The file format is plain text with comma separated values (CSV).
 def import_inventory(inventory, filename="import_inventory.csv"):
-    with open('import_inventory.csv', 'w', newline='') as f:
-        reader = csv.reader(f)
-    for row in reader:
-        print(row)
-
+    path = "/home/codeorgo/Documents/2nd_SI_Week/pbwp-1st-si-game-inventory-weiliya/import_inventory.csv"
+    file = open(path, newline='')
+    reader = csv.reader(file)
+    data = [row for row in reader]
+    print(data)
 
 # Exports the inventory into a .csv file.
 # if the filename argument is None it creates and overwrites a file
 # called "export_inventory.csv". The file format is the same plain text 
 # with comma separated values (CSV).
 def export_inventory(inventory, filename="export_inventory.csv"):
-    pass
-
+    returns_path = "/home/codeorgo/Documents/2nd_SI_Week/pbwp-1st-si-game-inventory-weiliya/import_inventory.csv"
+    file = open(returns_path, 'w')
+    writer = csv.writer(file)
+    writer.writerow(["ruby", "arrow"])
+    
+export_inventory(inventory, filename)
